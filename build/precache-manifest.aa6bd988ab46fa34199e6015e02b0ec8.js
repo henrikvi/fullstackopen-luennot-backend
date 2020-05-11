2fe91c04952b1d9308dc6e0ca3dc68db2b98876c
henrikvi <hjviluksela@gmail.com>
2020-05-11 19:59:14 +0300

    add frontend (build)

---
+++ build/precache-manifest.aa6bd988ab46fa34199e6015e02b0ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c32d345fe9647d087fdd4e6638a5b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3edf196613f3a0a4d01",
+    "url": "/static/css/main.2e428876.chunk.css"
+  },
+  {
+    "revision": "50e2229253b178dd227b",
+    "url": "/static/js/2.d1033c6c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d1033c6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3edf196613f3a0a4d01",
+    "url": "/static/js/main.dc3768ef.chunk.js"
+  },
+  {
+    "revision": "810b7a84121f49ea6cd3",
+    "url": "/static/js/runtime-main.6d2dcca1.js"
+  }
+]);
